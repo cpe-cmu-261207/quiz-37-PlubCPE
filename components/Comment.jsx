05e5571433c3e24595e40ef496319fd67154ca3b
PlubCPE <chakkapop_s@cmu.ac.th>
2022-09-15 14:32:58 +0700

    finish

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Reply from "./Reply";
+import { comments } from "../libs/comments";
 
 export default function Comment(props) {
+  const [Comment, setComment] = useState("");
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={props.userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{props.username}</span>
+          <br />
+          <span>{props.commentText}</span>
+          {props.likeNum > 0 && (
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{`${props.likeNum} คน`}</span>
+            </div>
+          )}
+        </div>
+      </div>
 
       {/*render Reply here... */}
+      {props.replies.map((x, i) => {
+        return (
+          <Reply
+            key={i}
+            username={x.username}
+            userImagePath={x.userImagePath}
+            replyText={x.replyText}
+            likeNum={x.likeNum}
+          />
+        );
+      })}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Comment from "../components/Comment";
 import PostOwner from "../components/PostOwner";
+import Reply from "../components/Reply";
 import { comments } from "../libs/comments";
 
 export default function Home() {
@@ -14,7 +15,13 @@ export default function Home() {
         className="mx-auto bg-white p-3 rounded rounded-3 shadow-sm"
       >
         {/* PostOwner Example*/}
-        <div className="vstack gap-3">
+        <PostOwner
+          name="Atipat Daowraeng 640610674"
+          text="Quiz ง่ายจังเลยขอยากกว่านี้ได้ไหมครับ #261207"
+          like="786"
+          userImagePath="/profileImages/myprofile_n.jpg"
+        />
+        {/* <div className="vstack gap-3">
           <div className="d-flex align-items-center gap-3">
             <img
               src="/profileImages/handsome.jpg"
@@ -31,10 +38,22 @@ export default function Home() {
             <span className="text-muted">100 คน</span>
           </div>
           <hr className="m-0" />
-        </div>
+        </div> */}
 
         {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
+        {comments.map((x, i) => {
+          return (
+            <Comment
+              key={i}
+              username={x.username}
+              userImagePath={x.userImagePath}
+              commentText={x.commentText}
+              likeNum={x.likeNum}
+              replies={x.replies}
+            />
+          );
+        })}
+        {/* <div className="d-flex gap-2 my-2">
           <img
             src="/profileImages/lisa.jpg"
             width="48"
@@ -54,10 +73,11 @@ export default function Home() {
               <span className="text-muted">999 คน</span>
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
+
+        {/* <div className="d-flex gap-2 my-2 ps-5">
           <img
             src="/profileImages/puppy.jpg"
             width="48"
@@ -73,7 +93,7 @@ export default function Home() {
             <br />
             <span>จริงค้าบบบบบบบบ</span>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
